aab93d2b86691ec2ddcd408f1c301c1025b02723
usmankhantrg <usmankhantrg@gmail.com>
2024-07-29 22:06:19 +0100

    cSharpMethods

---
+++ cSharpMethods/cSharpMethods/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace cSharpMethods
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program=new Program();
+            program.EvenNumbers();
+            Program.oddNumbers();
+
+        }
+        public void EvenNumbers()
+        {
+            int start = 0;
+            while(start < 10)
+            {
+                Console.WriteLine(start);
+                start += 2;
+            }
+        }
+        public static void oddNumbers()
+        {
+            int start = 1;
+            while(start <=10)
+            {
+                Console.WriteLine(start);
+                start += 2;
+            }
+        }
+    }
+
+}
