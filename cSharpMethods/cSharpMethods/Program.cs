963a83f3cd39d8dffa7d99fc6a4cba86e2a81e88
usmankhantrg <usmankhantrg@gmail.com>
2024-07-30 05:49:37 +0100

    method parameters-2

--- cSharpMethods/cSharpMethods/Program.cs
+++ cSharpMethods/cSharpMethods/Program.cs
@@ -10,6 +10,15 @@ namespace cSharpMethods
             Program program=new Program();
             program.EvenNumbers();
             Program.oddNumbers();
+            valParameter(15);
+            int a = 15;
+            refParameter(ref a);
+            int sumResult = 0;
+            outParameter(10, 20, out sumResult);
+            Console.WriteLine("Out Parameter Sum: " + sumResult);
+            paramArrayParameter(1,2,3);
+            int[] param=new int[] {1,2,3,4};
+            paramArrayParameter(param);
 
         }
         public void EvenNumbers()
@@ -29,6 +38,26 @@ namespace cSharpMethods
                 Console.WriteLine(start);
                 start += 2;
             }
+        }
+        public static void valParameter(int b)
+        {
+            
+            Console.WriteLine("value parameter: " + b);
+        }
+
+        public static void refParameter(ref int b)
+        {
+            b = 20;
+            Console.WriteLine("Ref Parameter:" + b);
+        }
+        public static void outParameter(int FN, int SN, out int Result)
+        {
+            Result = FN + SN;
+        }
+
+        public static void paramArrayParameter(params int[] param)
+        {
+            Console.WriteLine("Params Array Length: "+param.Length);
         }
     }
 
