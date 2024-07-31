6dcac2e7d4208e54d6c427713a39726204ad85ee
usmankhantrg <usmankhantrg@gmail.com>
2024-08-01 00:06:15 +0100

    class

---
+++ cSharpClass/cSharpClass/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace cSharpClass
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee("John","Doe");
+            {
+                employee.PrintName();
+            }
+        }
+
+    }
+    class Employee
+    {
+        public string _firstName;
+        public string _lastName;
+        public Employee(string FirstName,string LastName)
+        {
+            this._firstName = FirstName;
+            this._lastName = LastName;
+
+            
+        }
+        public void PrintName()
+        {
+            Console.WriteLine("First Name: {0}, Last Name: {1}",this._firstName,this._lastName);
+        }
+    }
+}
