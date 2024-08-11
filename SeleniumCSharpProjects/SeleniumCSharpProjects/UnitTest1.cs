c8c2f10147fe19c5aea3d7c31391246269d2519d
usmankhantrg <usmankhantrg@gmail.com>
2024-08-11 20:39:06 +0100

    Selenium

---
+++ SeleniumCSharpProjects/SeleniumCSharpProjects/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SeleniumCSharpProjects
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
